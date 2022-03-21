68ac5940e2a677d9a2968c70ea766dd01c134b9c
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2022-03-21 09:02:31 +0700

    Menambahkan separator dan placeholder pada halaman input

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -20,12 +20,14 @@
         <label for="">EMAIL / PHONE NUMBER</label>
         <div class="inputWrapper">
           <img src="/images/icons/user.png" alt="" srcset="" class="left-icon">
-          <input type="text" id="inputEmail">
+          <img src="/images/icons/separator.png" alt="" class="separator">
+          <input type="text" id="inputEmail" placeholder="email / phone number">
         </div>
         <label for="">PASSWORD</label>
         <div class="inputWrapper">
           <img src="/images/icons/padlock.png" alt="" srcset="" class="left-icon">
-          <input type="password" id="inputPassword">
+          <img src="/images/icons/separator.png" alt="" class="separator">
+          <input type="password" id="inputPassword" placeholder="password">
           <img src="/images/icons/hide.png" alt="" srcset="" class="right-icon" id="passwordToggle">
         </div>
         <button type="submit">Login</button>
