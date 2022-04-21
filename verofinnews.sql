8306b7e315ac71a82c163b391f1dacd47b6e1ea2
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2022-04-21 15:37:56 +0700

    update database

--- verofinnews.sql
+++ verofinnews.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 18, 2022 at 05:32 AM
+-- Generation Time: Apr 21, 2022 at 10:37 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -20,6 +20,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `verofinnews`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reset_password_token`
+--
+
+CREATE TABLE `reset_password_token` (
+  `user_id` int(11) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -42,17 +55,33 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `phone_number`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'Naufal Muafa', 'naufalmuafa77@gmail.com', '082322286759', '$2y$10$aL8KZ4Xv.pmZbC19HXoN2uT9t1QtFP1hT9k7RxiEopamQmI.OUViK', '2022-04-18 02:48:27', '2022-04-18 02:48:27');
+(1, 'Naufal Muafa', 'naufalmuafa77@gmail.com', '082322286759', '$2y$10$ZEx.q0xHuqEjutlFiGcbg.EdN..pUquQmVVV9u2UZw0AreXuVcASS', '2022-04-21 07:50:34', '2022-04-21 08:35:25');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `reset_password_token`
+--
+ALTER TABLE `reset_password_token`
+  ADD KEY `reset_password_token.user_id.users.id` (`user_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reset_password_token`
+--
+ALTER TABLE `reset_password_token`
+  ADD CONSTRAINT `reset_password_token.user_id.users.id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
