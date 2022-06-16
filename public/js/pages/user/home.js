16e9e9dfb5547f38252aa35fa545a16ea1f33bdb
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2022-06-16 10:06:18 +0700

    Memfungsikan menu kategori

--- public/js/pages/user/home.js
+++ public/js/pages/user/home.js
@@ -2,6 +2,10 @@ console.log('home');
 
 const newsContainer = document.getElementById('news-list');
 
+const clearNewsList = () => {
+  newsContainer.innerHTML = '';
+};
+
 const displayAllNews = async () => {
   let response = await fetch('http://127.0.0.1:5000/get_all_news');
   response = await response.text();
@@ -24,8 +28,52 @@ const displayAllNews = async () => {
   }
 };
 
-const init = () => {
-  displayAllNews();
+// eslint-disable-next-line no-unused-vars
+const onCategoryClickListener = async (selectedCategory) => {
+  let response = await fetch(`http://127.0.0.1:5000/get_news_by_category?category=${selectedCategory}`);
+  response = await response.text();
+  let news = JSON.parse(response);
+  news = news.news;
+
+  clearNewsList();
+  // eslint-disable-next-line no-restricted-syntax
+  for (item of news) {
+    const template = document.getElementById('newsTemplate');
+
+    const category = convertCategoryToReadable(item.category);
+
+    const clone = template.content.cloneNode(true);
+    clone.querySelector('a').setAttribute('href', `/baca?id=${item.id}`);
+    clone.querySelector('img').setAttribute('src', item.poster);
+    clone.querySelector('p').innerHTML = item.title;
+    clone.querySelector('.category').innerHTML = category;
+    clone.querySelector('.time').innerHTML = parseDateFromBackEnd(item.date, item.weekday);
+
+    newsContainer.append(clone);
+  }
+
+  const categoryMenuElement = document.getElementsByClassName('category-menu');
+  // eslint-disable-next-line no-restricted-syntax
+  for (const menuElement of categoryMenuElement) {
+    if (menuElement.getAttribute('data-category') !== selectedCategory) {
+      menuElement.classList.remove('selected');
+    }
+    else {
+      menuElement.classList.add('selected');
+    }
+  }
+};
+
+const init = async () => {
+  // const categoryMenuElement = document.getElementsByClassName('category-menu');
+  // // eslint-disable-next-line no-restricted-syntax
+  // for (const menuElement of categoryMenuElement) {
+  //   menuElement.addEventListener('click', () => {
+  //     await onCategoryClickListener();
+  //   });
+  // }
+
+  // displayAllNews();
 };
 
 init();
--- resources/views/pages/user/home.blade.php
+++ resources/views/pages/user/home.blade.php
@@ -11,7 +11,19 @@
 @endpush
 
 @section('content')
-  @include('includes.head')
+  <div id="head">
+    <div class="left-side">
+      <p>Category</p>
+      <img src="/images/icons/separator.png" class="separator" alt="ikon separator" srcset="">
+      <p onclick="onCategoryClickListener('hiburan')" class="category-menu" data-category="hiburan">Hiburan</p>
+      <p onclick="onCategoryClickListener('gaya_hidup')" class="category-menu" data-category="gaya_hidup">Gaya Hidup</p>
+    </div>
+    <div class="right-side">
+      <input type="text" name="" id="" placeholder="Search">
+      <img src="/images/icons/separator.png" class="separator" alt="ikon separator" srcset="">
+      <img src="/images/icons/lup.png" alt="ikon lup" srcset="" class="ic-lup">
+    </div>
+  </div>
   <main id="news-list"></main>
   @include('includes.newsTemplate')
 @endsection
